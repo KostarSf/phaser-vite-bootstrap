87cf28200b8730b8e79ec16c267c5acc3c278a10
Maksim Peskov <kostarsf@gmail.com>
2023-08-19 10:59:44 +0300

    Replace separate functions to scene class

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,32 @@
 import Phaser from "phaser";
 
-const preload: Phaser.Types.Scenes.ScenePreloadCallback = function () {
-  this.load.setBaseURL("https://labs.phaser.io");
+class Example extends Phaser.Scene {
+  preload() {
+    this.load.setBaseURL("https://labs.phaser.io");
 
-  this.load.image("sky", "assets/skies/space3.png");
-  this.load.image("logo", "assets/sprites/phaser3-logo.png");
-  this.load.image("red", "assets/particles/red.png");
-};
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.image("red", "assets/particles/red.png");
+  }
 
-const create: Phaser.Types.Scenes.SceneCreateCallback = function () {
-  this.add.image(400, 300, "sky");
+  create() {
+    this.add.image(400, 300, "sky");
 
-  const emitter = this.add.particles(undefined, undefined, "red", {
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: "ADD",
-  });
+    const emitter = this.add.particles(undefined, undefined, "red", {
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: "ADD",
+    });
 
-  const logo = this.physics.add.image(400, 100, "logo");
+    const logo = this.physics.add.image(400, 100, "logo");
 
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
 
-  emitter.startFollow(logo);
-};
+    emitter.startFollow(logo);
+  }
+}
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -36,7 +38,7 @@ const config: Phaser.Types.Core.GameConfig = {
       gravity: { y: 200 },
     },
   },
-  scene: { preload, create },
+  scene: Example,
 };
 
 const game = new Phaser.Game(config);
